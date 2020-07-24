13ab38180725d96f92c7d6821d51f69e9c226bf1
GioF71 <giovanni.fulco@gmail.com>
2020-07-24 10:29:08 +0200

    added run-watchtower script

---
+++ run-watchtower.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+docker stop watchtower
+docker rm watchtower
+
+docker pull containrrr/watchtower:latest
+
+docker run -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --restart unless-stopped \
+  --name watchtower-portainer \
+  containrrr/watchtower
+  
