40c59903297fbefa759df4b72c82c76423a12581
GioF71 <giovanni.fulco@gmail.com>
2020-07-24 10:45:37 +0200

    line breaks

--- run.sh
+++ run.sh
@@ -9,7 +9,8 @@ docker pull portainer/portainer:latest
 docker run -d \
   -p 8000:8000 \
   -p 9000:9000 \
-  -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data \
   --restart unless-stopped \
   --name portainer \
   portainer/portainer
