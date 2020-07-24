70d8b3efd7e60fc08253adb0c4885b5eff45edb8
GioF71 <giovanni.fulco@gmail.com>
2020-07-24 10:11:59 +0200

    cleanup

--- run.sh
+++ run.sh
@@ -6,4 +6,10 @@ docker rm portainer
 docker volume create portainer_data
 docker pull portainer/portainer:latest
 
-docker run -d -p 0.0.0.0:8000:8000 -p 0.0.0.0:9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart unless-stopped --name portainer portainer/portainer
+docker run -d \
+  -p 0.0.0.0:8000:8000 \
+  -p 0.0.0.0:9000:9000 \
+  -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data \
+  --restart unless-stopped \
+  --name portainer \
+  portainer/portainer
