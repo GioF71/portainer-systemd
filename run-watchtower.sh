c68998d4a5f7bae0cadfa59d9b93ce660eed4283
GioF71 <giovanni.fulco@gmail.com>
2020-11-19 01:26:18 +0100

    Updates to run-watchtower: timezone and more relaxed poll interval (10h)

--- run-watchtower.sh
+++ run-watchtower.sh
@@ -7,6 +7,8 @@ docker pull containrrr/watchtower:latest
 
 docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /etc/timezone:/etc/timezone:ro \
+  --e WATCHTOWER_POLL_INTERVAL=36000 \
   --restart unless-stopped \
   --name watchtower \
   containrrr/watchtower
